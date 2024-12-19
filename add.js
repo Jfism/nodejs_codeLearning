0f219e49f6cce2aa7864dccdb23fe78c687f68de
Jafar Ismai Mohamed <121930953+Jfism@users.noreply.github.com>
2024-12-19 19:24:34 +0500

    Add files via upload
    module exports

---
+++ add.js
@@ -0,0 +1,12 @@
+const add = (a,b)=>{
+    return a + b ;
+
+}
+
+const sum  = add(1,2);
+console.log(sum);
+
+const sum1  = add(2,2);
+console.log(sum1);
+
+module.export = add; 
---
+++ index.js
@@ -0,0 +1,8 @@
+//local  modul
+ const add  = require("/add");
+console.log("hello from index.js ");
+const sum  = add(1,2);
+console.log(sum);
+
+const sum1  = add(2,2);
+console.log(sum1);
